ca895b45538410bdb2d24828a8610c2cc5a0a7f8
ccwong0 <58868802+ccwong0@users.noreply.github.com>
2023-07-24 20:32:25 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Load JSON data and populate the table
+fetch("car_data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const carTable = document.getElementById("carTable");
+    const carManufacturers = data.carManufacturers;
+
+    carManufacturers.forEach((car) => {
+      const row = carTable.insertRow();
+      const manufacturerCell = row.insertCell(0);
+      const modelsCell = row.insertCell(1);
+
+      manufacturerCell.innerText = car.manufacturer;
+      modelsCell.innerText = car.models.join(", ");
+    });
+  })
+  .catch((error) => console.error("Error loading JSON data:", error));
